06aae85df352a7edaf9107b6d86db9d920e758d7
higorxb <higorxb@gmail.com>
2015-03-23 19:50:03 -0300

    At2

--- T2/t2.c
+++ T2/t2.c
@@ -3,6 +3,25 @@
 #include <string.h>
 
 int main (int argc,char** argv){
+    char *nome;
+    nome=(char*)malloc(50*sizeof(char));
+    if(nome==NULL){
+        printf("\nErro na alocacao de memoria.\n");
+        exit(1);
+    }
+    float *medias;
+    medias=(float*)malloc(50*sizeof(float));
+    if(medias==NULL){
+        printf("\nErro na alocacao de memoria.\n");
+        exit(1);
+    }
+    int *matri,*n,i,j;
+    n=matri=(int*)malloc(50*sizeof(int));
+    if(n==NULL || matri==NULL ){
+        printf("\nErro na alocacao de memoria.\n");
+        exit(1);
+    }
+    char* nomes;
 
 
 }
