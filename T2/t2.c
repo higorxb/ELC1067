bcf5f948c53a70b91a48f5e3001f1eb27af3fed6
higorxb <higorxb@gmail.com>
2015-03-23 20:35:24 -0300

    At4

--- T2/t2.c
+++ T2/t2.c
@@ -3,8 +3,31 @@
 #include <string.h>
 
 void leitor_alunos(int* matri,char* nomes,int* n ){
-
-
+    int mat,i,linha;
+    char c;
+    char *nome;
+    nome=(char*)malloc(50*sizeof(char));
+    if(nome==NULL){
+        printf("\nErro na alocacao de memoria.\n");
+        exit(1);
+    }
+    FILE*f = fopen("alunos.txt", "r");
+    linha = 0;
+    while(feof(f) == 0){
+        if(fscanf(f, "%d", &mat) <= 0)
+        break;
+        i = 0;
+        c = fgetc(f);
+        while(c != '\n' && feof(f)==0){
+            nome[i] = c;
+            c = fgetc(f);
+            i++;
+        }
+        nome[i] = '\0';
+        matri[linha]=mat;
+        strcpy(nomes[linha],nome);
+        linha++;
+    }
 }
 
 
